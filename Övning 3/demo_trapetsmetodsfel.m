a53e095ef4dbf63f158b8ca670bb7262ca6502a0
ludviglundgrens <48127020+ludviglundgrens@users.noreply.github.com>
2021-08-24 17:09:40 +0200

    Update demo_trapetsmetodsfel.m

--- Övning 3/demo_trapetsmetodsfel.m
+++ Övning 3/demo_trapetsmetodsfel.m
@@ -1,33 +1,36 @@
+a = 0;
+b = 10;
+
 f = @(x) 25-(x).^2;
 
-x = 0:0.01:5;
+x = a:0.01:b;
 fx = f(x);
 
 plot(x, fx)
 hold on
 
 % ett steg
-x2 = 0:5:5;
+x2 = a:5:b;
 fx2 = f(x2);
 plot(x2, fx2)
 
 % två steg
-x3 = 0:5/2:5;
+x3 = a:5/2:b;
 fx3 = f(x3);
 plot(x3, fx3)
 
 % fyra steg
-x4 = 0:5/4:5;
+x4 = a:5/4:b;
 fx4 = f(x4);
 plot(x4, fx4)
 hold off
 
 %% plotta felet
-error1 = fx-interp1(x3, fx3, x);
+error1 = fx-interp1(x2, fx2, x);
 plot(x, error1)
 hold on
 
-error2 = fx-interp1(x2, fx2, x);
+error2 = fx-interp1(x3, fx3, x);
 plot(x, error2)
 
 error3 = fx-interp1(x4, fx4, x);
